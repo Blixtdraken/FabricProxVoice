bf21baf118817840e228de093b409b19ab82c9d4
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 13:09:29 +0100

    Adding all basic files

---
+++ src/main/java/com/github/fabricproxvoice/fabricproxvoice/FabricProxVoice.java
@@ -0,0 +1,11 @@
+package com.github.fabricproxvoice.fabricproxvoice;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class FabricProxVoice implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/fabricproxvoice/fabricproxvoice/client/FabricproxvoiceClient.java
@@ -0,0 +1,14 @@
+package com.github.fabricproxvoice.fabricproxvoice.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FabricproxvoiceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
